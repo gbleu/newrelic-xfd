e6f20955306b9c74a8b0b862bdce2403bc50a8d7
gbleu <bleu@beex.fr>
2014-02-18 17:27:11 +0100

    fix debug

--- server.js
+++ server.js
@@ -11,21 +11,21 @@ var app = connect()
     util.log("Incoming request");
     
     if (req.body) {
-	
-	  if(config.debug) { util.log(util.format("Body : %j", req.body)); }
-	  
-	  if (req.body.alert) {
-        var alert = JSON.parse(req.body.alert);
+    
+      if(config.debug) { util.log(util.format("Body : %j", req.body)); }
       
+      if (req.body.alert) {
+        var alert = JSON.parse(req.body.alert);
+        
         var msg = alert.short_description;
-
+        
         say.speak('voice_kal_diphone', msg, function () {
-          util.log('%s tts complete', msg);
+          util.log(util.format('%s tts complete', msg));
         });
       }
-
+      
       res.end('success');
-	}
+    }
   });
 
 http.createServer(app).listen(config.port);
